a9a6f065925e8de07d50870d2fd1ea81249f4a56
biplob-lab <biplobmojumder@gmail.com>
2026-08-01 16:15:42 +0600

    Add nested list creation examples and print statements

--- nested_list.py
+++ nested_list.py
@@ -1,2 +1,12 @@
 my_favourite_list= ["football","ROnaldo",["Gta v","Last of US"]]
 print(my_favourite_list[2][0])
+m=[]
+""" Without nested loop
+for i in range(5):#(0,1,2,3,4)
+    m.append([])#creat empty list
+    for j in range(5):
+       m[i].append(j)
+print(m) """
+#List comprehension
+m = [[c for c in range(5)] for b in range(5)]
+print (m)
