8b3b3c729d032252706097d7ad48c9eeb4588f5b
biplob-lab <biplobmojumder@gmail.com>
2026-08-02 02:30:04 +0600

    Refactor nested list examples and improve code comments

--- nested_list.py
+++ nested_list.py
@@ -1,12 +1,20 @@
-my_favourite_list= ["football","ROnaldo",["Gta v","Last of US"]]
+""" my_favourite_list= ["football","ROnaldo",["Gta v","Last of US"]]
 print(my_favourite_list[2][0])
-m=[]
+m=[] """
 """ Without nested loop
 for i in range(5):#(0,1,2,3,4)
     m.append([])#creat empty list
     for j in range(5):
        m[i].append(j)
 print(m) """
-#List comprehension
+""" #List comprehension
 m = [[c for c in range(5)] for b in range(5)]
-print (m)
+print (m) """
+
+""" m = [[2,3,5],[4,9,11],[6,3,5]]
+odds = [e for r in  m for e in r if e %2 !=0]
+print (set(odds)) """
+
+
+
+
