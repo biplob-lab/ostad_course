dcc18b660ad8d973dd5676832d2e7552b0b5dd45
ASUS <biplobmojumder123456@gmail.com>
2026-08-06 03:05:09 +0600

    Add while loop examples to loop.py for demonstration purposes

---
+++ loop.py
@@ -0,0 +1,19 @@
+#while loop
+""" i = 3
+while i !=0:
+    print("siuuuu")
+    i = i-1 """
+
+""" i = 1
+while i <= 3:
+    print ("siuuu")
+    i = i+1
+ """
+
+""" i = 0
+while i<3:
+    print("siuu")
+    i+= 1
+ """
+
+#for loop
